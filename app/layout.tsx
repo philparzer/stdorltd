773c9839ee18d245162d98481ab15475176a7f70
Philipp Parzer <phil0898.pp@gmail.com>
2024-05-28 20:29:15 +0200

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "LTD or STD",
-  description: "Is the abbreviation a limited liability company in Europe or a medical condition?",
+  description:
+    "Is the abbreviation a limited liability company in Europe or a medical condition?",
 };
 
 export default function RootLayout({
@@ -16,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-zinc-100`}>{children}</body>
+      <body className={`${inter.className} bg-zinc-100`}>
+        {children} <Analytics />
+      </body>
     </html>
   );
 }
